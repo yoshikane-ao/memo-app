f90321fe95189bcf2608a32f640208ede9028fb6
ao <popoponpop@gmail.com>
2026-03-24 18:48:46 +0900

    config.ts修正

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,12 +5,12 @@ import type { SignOptions, Secret } from "jsonwebtoken";
 
 function mustGet(name: string): string {
   const v = process.env[name];
-  if (!v) throw new Error(`ENV NULL is required`);
+  if (!v) throw new Error(`ENV ${name} is required`);
   return v;
 }
 
 export const config = {
   port: Number(process.env.PORT ?? 3004),
   jwtSecret: mustGet("JWT_SECRET") as Secret,
-  jwtExpiresIn: (process.env.JWT_EXPIRES_IN="1h") as SignOptions["expiresIn"],
+  jwtExpiresIn: (process.env.JWT_EXPIRES_IN ?? "1h") as SignOptions["expiresIn"],
 };
