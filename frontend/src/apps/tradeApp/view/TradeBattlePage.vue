cbd3c9972844a91cc8aefa8bf9c7598093c07c64
Kai Hasegawa <kai@hase-studio.com>
2026-03-30 09:35:48 +0900

    import修正

--- frontend/src/apps/tradeApp/view/TradeBattlePage.vue
+++ frontend/src/apps/tradeApp/view/TradeBattlePage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useRouter } from 'vue-router'
-import { useTradeGameStore } from 'frontend/src/apps/tradeApp/store/useTradeGameStore.ts'
+import { useTradeGameStore } from '../store/useTradeGameStore'
 
 const router = useRouter()
 const gameStore = useTradeGameStore()
